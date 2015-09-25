75eebdcd05a2356351a086816cd99689ff88460b
Brice Shatzer <brice.shatzer@gmail.com>
2015-09-25 18:32:24 -0500

    continued work

--- js/nlform.js
+++ js/nlform.js
@@ -33,10 +33,17 @@
 				self.fldOpen++;
 				self.fields.push( new NLField( self, el, 'dropdown', self.fldOpen ) );
 			} );
-			Array.prototype.slice.call( this.el.querySelectorAll( 'input:not([type="hidden"])' ) ).forEach( function( el, i ) {
+
+			Array.prototype.slice.call( this.el.querySelectorAll( 'input.modal' ) ).forEach( function( el, i ) {
 				self.fldOpen++;
 				self.fields.push( new NLField( self, el, 'input', self.fldOpen ) );
 			} );
+
+			Array.prototype.slice.call( this.el.querySelectorAll( 'input.income' ) ).forEach( function( el, i ) {
+				self.fldOpen++;
+				self.fields.push( new NLField( self, el, 'income', self.fldOpen ) );
+			} );			
+
 			this.overlay.addEventListener( 'click', function(ev) { self._closeFlds(); } );
 			this.overlay.addEventListener( 'touchstart', function(ev) { self._closeFlds(); } );
 		},
@@ -61,9 +68,15 @@
 			if( this.type === 'dropdown' ) {
 				this._createDropDown();	
 			}
+/*==*/
 			else if( this.type === 'input' ) {
 				this._createInput();	
 			}
+
+			else if( this.type === 'income' ) {
+				this._createIncome();	
+			}
+/*==*/
 		},
 		_createDropDown : function() {
 			var self = this;
@@ -87,6 +100,7 @@
 			this.elOriginal.parentNode.insertBefore( this.fld, this.elOriginal );
 			this.elOriginal.style.display = 'none';
 		},
+/*==*/
 		_createInput : function() {
 			var self = this;
 			this.fld = document.createElement( 'div' );
@@ -116,6 +130,45 @@
 			this.elOriginal.parentNode.insertBefore( this.fld, this.elOriginal );
 			this.elOriginal.style.display = 'none';
 		},
+		_createIncome : function() {
+			var self = this;
+			this.fld = document.createElement( 'div' );
+			this.fld.className = 'nl-field nl-ti-text';
+			this.toggle = document.createElement( 'a' );
+			this.toggle.className = 'nl-field-toggle';			
+			if(this.elOriginal.getAttribute('value')){				
+				this.toggle.innerHTML = this.elOriginal.getAttribute('value');
+			} else { 
+				this.toggle.className += ' placeholder';
+				this.toggle.innerHTML = this.elOriginal.getAttribute('placeholder');
+			}
+			//this.toggle.innerHTML = this.elOriginal.getAttribute('value')? this.elOriginal.getAttribute('value'): this.elOriginal.getAttribute('placeholder');
+			//this.toggle.className = 'nl-field-toggle';
+			this.optionsList = document.createElement( 'ul' );
+			this.getinput = document.createElement( 'input' );
+			this.getinput.setAttribute( 'type', 'text');
+			this.getinput.setAttribute( 'placeholder', this.elOriginal.getAttribute( 'placeholder' ) );
+			this.getinput.setAttribute( 'value', this.elOriginal.getAttribute('value')? this.elOriginal.getAttribute('value'): '');
+			
+			this.getinputWrapper = document.createElement( 'li' );
+			this.getinputWrapper.className = 'nl-ti-input';
+			this.inputsubmit = document.createElement( 'button' );
+			this.inputsubmit.className = 'nl-field-go';
+			this.inputsubmit.innerHTML = 'Go';
+			this.getinputWrapper.appendChild( this.getinput );
+			this.getinputWrapper.appendChild( this.inputsubmit );
+			this.example = document.createElement( 'li' );
+			this.example.className = 'nl-ti-example';
+			this.example.innerHTML = this.elOriginal.getAttribute( 'data-subline' );
+			this.optionsList.appendChild( this.getinputWrapper );
+			this.optionsList.appendChild( this.example );
+			this.fld.appendChild( this.toggle );
+			this.fld.appendChild( this.optionsList );
+			this.elOriginal.parentNode.insertBefore( this.fld, this.elOriginal );
+			this.elOriginal.style.display = 'none';
+			debugger
+		},
+/*==*/
 		_initEvents : function() {
 			var self = this;
 			this.toggle.addEventListener( 'click', function( ev ) { ev.preventDefault(); ev.stopPropagation(); self._open(); } );
@@ -128,7 +181,8 @@
 					el.addEventListener( 'touchstart', function( ev ) { ev.preventDefault(); self.close( el, opts.indexOf( el ) ); } );
 				} );
 			}
-			else if( this.type === 'input' ) {
+/*==*/
+			else if( this.type === 'input' || 'income' ) {
 				this.getinput.addEventListener( 'keydown', function( ev ) {
 					if ( ev.keyCode == 13 ) {
 						self.close();
@@ -137,6 +191,7 @@
 				this.inputsubmit.addEventListener( 'click', function( ev ) { ev.preventDefault(); self.close(); } );
 				this.inputsubmit.addEventListener( 'touchstart', function( ev ) { ev.preventDefault(); self.close(); } );
 			}
+/*==*/
 
 		},
 		_open : function() {
@@ -169,11 +224,27 @@
 					this.elOriginal.value = this.elOriginal.children[ this.selectedIdx ].value;
 				}
 			}
+			/*==*/
 			else if( this.type === 'input' ) {
 				this.getinput.blur();
 				this.toggle.innerHTML = this.getinput.value.trim() !== '' ? this.getinput.value : this.getinput.getAttribute( 'placeholder' );
 				this.elOriginal.value = this.getinput.value;
 			}
+			else if( this.type === 'income' ) {
+				var displayedIncome;
+				var n = this.getinput.value.trim().replace(/(,|\$)/, '');
+				this.getinput.blur();
+				this.toggle.className = 'nl-field-toggle';
+				if( !isNaN(parseFloat(n)) && isFinite(n) ){						
+					displayedIncome = parseInt(n).toLocaleString('en-US',{style: 'currency', currency: 'USD'});
+				} else {
+					this.toggle.className += ' placeholder';
+					displayedIncome = this.getinput.getAttribute( 'placeholder' );
+				}
+				this.toggle.innerHTML = displayedIncome;
+				this.elOriginal.value = this.getinput.value;
+			}			
+			/*==*/
 		}
 	};
 
